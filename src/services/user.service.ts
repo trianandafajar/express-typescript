1ddc3678258c2cc5e3ee3af3f0a28e0910afa59e
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-26 22:16:12 +0700

    Update Service User

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,22 +1,24 @@
-import type UserType from '../types/user.type'
+import type { UserType } from '../types/user.type'
 import prisma from '../utils/client'
 
+// Service untuk membuat user baru
 export const createUser = async (payload: UserType): Promise<UserType> => {
-  const data = await prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       ...payload
     }
   })
-  return data
+  return user
 }
 
+// Service untuk login (mencari user berdasarkan email)
 export const userLogin = async (
   payload: UserType
 ): Promise<UserType | null> => {
-  const data = await prisma.user.findUnique({
+  const user = await prisma.user.findUnique({
     where: {
       email: payload.email
     }
   })
-  return data
+  return user
 }
