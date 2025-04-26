fb2fc861a701e48fb447c53918984dba6e248220
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-26 22:16:01 +0700

    Update Service Barang

--- src/services/barang.service.ts
+++ src/services/barang.service.ts
@@ -1,34 +1,35 @@
-import type BarangType from '../types/barang.type'
+import type { BarangType } from '../types/barang.type'
 import prisma from '../utils/client'
 
-export const getBarang = async (): Promise<BarangType[] | null> => {
-  const data = await prisma.barang.findMany()
-  return data
+// Ambil semua barang
+export const getBarang = async (): Promise<BarangType[]> => {
+  const barang = await prisma.barang.findMany()
+  return barang
 }
 
+// Ambil barang berdasarkan ID
 export const getBarangById = async (id: number): Promise<BarangType | null> => {
-  const data = await prisma.barang.findUnique({ where: { id } })
-  return data
+  const barang = await prisma.barang.findUnique({ where: { id } })
+  return barang
 }
 
-export const insertBarang = async (
-  payload: BarangType
-): Promise<BarangType> => {
-  const data = await prisma.barang.create({ data: payload })
-  return data
+// Insert barang baru
+export const insertBarang = async (payload: BarangType): Promise<BarangType> => {
+  const barang = await prisma.barang.create({ data: payload })
+  return barang
 }
 
-export const updateBarang = async (
-  payload: BarangType
-): Promise<BarangType> => {
-  const data = await prisma.barang.update({
+// Update barang
+export const updateBarang = async (payload: BarangType): Promise<BarangType> => {
+  const barang = await prisma.barang.update({
     where: { id: payload.id },
     data: { ...payload }
   })
-  return data
+  return barang
 }
 
+// Hapus barang
 export const deleteBarang = async (id: number): Promise<BarangType> => {
-  const data = await prisma.barang.delete({ where: { id } })
-  return data
+  const barang = await prisma.barang.delete({ where: { id } })
+  return barang
 }
