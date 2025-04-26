8e9f5d48f2fbeda921136743b47081e9182bc96c
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-26 22:17:10 +0700

    Update Route User

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -4,10 +4,16 @@ import {
   refreshToken,
   registerUser
 } from '../controllers/user.controller'
+
 const userRouter = Router()
 
+// POST /api/user/register
 userRouter.post('/register', registerUser)
+
+// POST /api/user/login
 userRouter.post('/login', loginUser)
+
+// GET /api/user/refresh
 userRouter.get('/refresh', refreshToken)
 
 export default userRouter
