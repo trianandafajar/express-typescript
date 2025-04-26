940e3d778bd33e28a71defcffb971a2497bb94ad
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-26 22:16:27 +0700

    Update Validation Barang

--- src/validations/barang.validation.ts
+++ src/validations/barang.validation.ts
@@ -1,30 +1,28 @@
-import joi from 'joi'
+import Joi from 'joi'
 import type { BarangType } from '../types/barang.type'
 
 export const inputBarangValidation = (
   payload: BarangType
-): joi.ValidationResult<BarangType> => {
-  const schema = joi
-    .object({
-      nama: joi.string().trim().required().messages({
-        'string.base': 'Barang harus berupa string',
-        'string.empty': 'Barang tidak boleh kosong',
-        'any.required': 'Barang harus diisi'
-      }),
-      jumlah: joi.number().strict().greater(0).required().messages({
-        'number.base': 'Jumlah harus berupa angka',
-        'number.empty': 'Jumlah tidak boleh kosong',
-        'number.greater': 'Jumlah harus lebih dari 0',
-        'any.required': 'Jumlah harus diisi'
-      }),
-      harga: joi.number().strict().greater(0).required().messages({
-        'number.base': 'Harga harus berupa angka',
-        'number.empty': 'Harga tidak boleh kosong',
-        'number.greater': 'Harga harus lebih dari 0',
-        'any.required': 'Harga harus diisi'
-      })
+): Joi.ValidationResult<BarangType> => {
+  const schema = Joi.object<BarangType>({
+    nama: Joi.string().trim().required().messages({
+      'string.base': 'Nama barang harus berupa string',
+      'string.empty': 'Nama barang tidak boleh kosong',
+      'any.required': 'Nama barang harus diisi'
+    }),
+    jumlah: Joi.number().strict().greater(0).required().messages({
+      'number.base': 'Jumlah harus berupa angka',
+      'number.empty': 'Jumlah tidak boleh kosong',
+      'number.greater': 'Jumlah harus lebih dari 0',
+      'any.required': 'Jumlah harus diisi'
+    }),
+    harga: Joi.number().strict().greater(0).required().messages({
+      'number.base': 'Harga harus berupa angka',
+      'number.empty': 'Harga tidak boleh kosong',
+      'number.greater': 'Harga harus lebih dari 0',
+      'any.required': 'Harga harus diisi'
     })
-    .strict()
+  }).strict()
 
   return schema.validate(payload, { abortEarly: false })
 }
