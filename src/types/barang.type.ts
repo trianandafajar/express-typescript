fa49c60726cbe67a870518be82015b95d9f4256e
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-08 23:58:09 +0700

    Update Barang

--- src/types/barang.type.ts
+++ src/types/barang.type.ts
@@ -1,10 +1,12 @@
 import { Decimal } from '@prisma/client/runtime/library'
 
-export default interface BarangType {
+export interface BarangType {
   id: number
   nama: string
   jumlah: number
-  harga: Decimal
-  created_at?: Date
-  updated_at?: Date
+  harga: Decimal | number // fleksibel, tergantung apakah digunakan di backend/frontend
+  created_at?: Date | string
+  updated_at?: Date | string
 }
+
+export default BarangType
