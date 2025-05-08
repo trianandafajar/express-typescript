6a1e011b15f5266092b8e32eb06a63dfa193541a
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-08 23:59:48 +0700

    Update Users Type

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -2,7 +2,8 @@ export default interface UserType {
   user_id: string
   email: string
   nama: string
-  password: string
-  confirmPassword?: string
+  password: string // hashed password
   role: string
+  created_at?: Date
+  updated_at?: Date
 }
